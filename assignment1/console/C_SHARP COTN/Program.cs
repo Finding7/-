a581aa74c2a86bd70ce29d602e156df3b3793e38
Finding7 <2105644683@qq.com>
2025-02-24 20:47:51 +0800

    Add files via upload
    第一次作业

---
+++ assignment1/console/C_SHARP COTN/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_SHARP_COTN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("请输入第一个数字：");
+            if (!double.TryParse(Console.ReadLine(), out double num1))
+            {
+                Console.WriteLine("错误：第一个数字输入无效。");
+                return;
+            }
+
+            Console.Write("请输入运算符（+、-、*、/）：");
+            string op = Console.ReadLine();
+            if (op != "+" && op != "-" && op != "*" && op != "/")
+            {
+                Console.WriteLine("错误：无效的运算符。");
+                return;
+            }
+
+            Console.Write("请输入第二个数字：");
+            if (!double.TryParse(Console.ReadLine(), out double num2))
+            {
+                Console.WriteLine("错误：第二个数字输入无效。");
+                return;
+            }
+
+            double result = 0;
+            bool Error = false;
+
+            switch (op)
+            {
+                case "+":
+                    result = num1 + num2;
+                    break;
+                case "-":
+                    result = num1 - num2;
+                    break;
+                case "*":
+                    result = num1 * num2;
+                    break;
+                case "/":
+                    if (num2 == 0)
+                    {
+                        Console.WriteLine("错误：除数不能为零。");
+                        Error = true;
+                    }
+                    else
+                    {
+                        result = num1 / num2;
+                    }
+                    break;
+            }
+
+            // 输出结果
+            if (!Error)
+            {
+                Console.WriteLine($"计算结果：{result}");
+            }
+        }
+    }
+}
---
+++ assignment1/windows/C_SHARP/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace C_SHARP
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            comboBox1.Items.Add("+");
+            comboBox1.Items.Add("-");
+            comboBox1.Items.Add("*");
+            comboBox1.Items.Add("/");
+            comboBox1.SelectedIndex = 0;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+
+        }
+     
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            if (!double.TryParse(textBox1.Text, out double num1))
+            {
+                MessageBox.Show("第一个数字输入错误");
+                return;
+            }
+
+            if (!double.TryParse(textBox2.Text, out double num2))
+            {
+                MessageBox.Show("第二个数字输入错误");
+                return;
+            }
+
+            string op = comboBox1.SelectedItem.ToString();
+
+            double result = 0;
+            switch (op)
+            {
+                case "+":
+                    result = num1 + num2;
+                    break;
+                case "-":
+                    result = num1 - num2;
+                    break;
+                case "*":
+                    result = num1 * num2;
+                    break;
+                case "/":
+                    if (num2 == 0)
+                    {
+                        MessageBox.Show("不能除以零");
+                        return;
+                    }
+                    result = num1 / num2;
+                    break;
+            }
+
+            label1.Text = "结果: " + result.ToString();
+        }
+    }
+}
