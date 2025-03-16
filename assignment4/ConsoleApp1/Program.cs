e927cc4b89fdfbbc78ba1cb4c407fadcd05a4ff1
Finding7 <2105644683@qq.com>
2025-03-16 20:39:35 +0800

    第四次作业
    第四次作业

---
+++ assignment4/ConsoleApp1/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+
+
+    // 链表节点
+    public class Node<T>
+    {
+        public Node<T> Next { get; set; }
+        public T Data { get; set; }
+
+        public Node(T t)
+        {
+            Next = null;
+            Data = t;
+        }
+    }
+
+    // 泛型链表类
+    public class GenericList<T>
+    {
+        private Node<T> head;
+        private Node<T> tail;
+
+        public GenericList()
+        {
+            tail = head = null;
+        }
+
+        public Node<T> Head
+        {
+            get { return head; }
+        }
+
+        public void Add(T t)
+        {
+            Node<T> n = new Node<T>(t);
+            if (tail == null)
+            {
+                head = tail = n;
+            }
+            else
+            {
+                tail.Next = n;
+                tail = n;
+            }
+        }
+
+        //ForEach方法
+        public void ForEach(Action<T> action)
+        {
+            Node<T> current = head;
+            while (current != null)
+            {
+                action(current.Data);
+                current = current.Next;
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 整型List
+            GenericList<int> intlist = new GenericList<int>();
+            for (int x = 0; x < 10; x++)
+            {
+                intlist.Add(x);
+            }
+
+            // 使用ForEach打印元素
+            Console.WriteLine("整数链表元素：");
+            intlist.ForEach(x => Console.WriteLine(x));
+
+            // 求和
+            int sum = 0;
+            intlist.ForEach(x => sum += x);
+            Console.WriteLine($"总和：{sum}");
+
+            // 求最大值
+            int max = int.MinValue;
+            intlist.ForEach(x => { if (x > max) max = x; });
+            Console.WriteLine($"最大值：{max}");
+
+            // 求最小值
+            int min = int.MaxValue;
+            intlist.ForEach(x => { if (x < min) min = x; });
+            Console.WriteLine($"最小值：{min}");
+
+            // 字符串型List
+            GenericList<string> strList = new GenericList<string>();
+            for (int x = 0; x < 10; x++)
+            {
+                strList.Add("str" + x);
+            }
+
+            // 使用ForEach打印字符串元素
+            Console.WriteLine("\n字符串链表元素：");
+            strList.ForEach(s => Console.WriteLine(s));
+        }
+    }
+    
+}
---
+++ assignment4/ConsoleApp2/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace ConsoleApp2
+{ 
+    public class AlarmEventArgs : EventArgs
+    {
+        public DateTime AlarmTime { get; }
+        public AlarmEventArgs(DateTime alarmTime) => AlarmTime = alarmTime;
+    }
+
+    public class AlarmClock
+    {
+        private readonly Timer _timer;
+        private DateTime _alarmTime;
+
+        public DateTime AlarmTime
+        {
+            get => _alarmTime;
+            set
+            {
+                if (value <= DateTime.Now)
+                    throw new ArgumentException("闹钟时间必须是未来的时间");
+                _alarmTime = value;
+            }
+        }
+
+        public delegate void AlarmEventHandler(object sender, AlarmEventArgs e);
+        public delegate void TickEventHandler(object sender, EventArgs e);
+
+        public event AlarmEventHandler Alarm;
+        public event TickEventHandler Tick;
+
+        public AlarmClock()
+        {
+            _timer = new Timer(1000);
+            _timer.Elapsed += OnTimerElapsed;
+            _timer.AutoReset = true;
+        }
+
+        private void OnTimerElapsed(object sender, ElapsedEventArgs e)
+        {
+            Tick?.Invoke(this, EventArgs.Empty);
+
+            if (DateTime.Now >= AlarmTime)
+            {
+                Alarm?.Invoke(this, new AlarmEventArgs(AlarmTime));
+                _timer.Stop(); // 触发后自动停止
+            }
+        }
+
+        public void Start() => _timer.Start();
+
+        public void SetNewAlarm(DateTime newTime)
+        {
+            _timer.Stop();
+            AlarmTime = newTime;
+            _timer.Start();
+            Console.WriteLine($"新闹钟已设置: {newTime:HH:mm:ss}");
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            var clock = new AlarmClock();
+
+            clock.Tick += (s, e) =>
+                Console.WriteLine($"嘀嗒... 当前时间：{DateTime.Now:HH:mm:ss}");
+
+            clock.Alarm += (s, e) =>
+                Console.WriteLine($"⏰ 时间到！设定的时间：{e.AlarmTime:HH:mm:ss}");
+
+            while (true)
+            {
+                Console.WriteLine("\n请输入闹钟时间（格式：HH mm，例如 14 30）（输入exit退出）：");
+                var input = Console.ReadLine();
+
+                if (string.IsNullOrEmpty(input)) continue;
+                if (input.ToLower() == "exit") break;
+
+                try
+                {
+                    var parts = input.Split();
+                    if (parts.Length != 2 ||
+                        !int.TryParse(parts[0], out int hour) ||
+                        !int.TryParse(parts[1], out int minute))
+                    {
+                        throw new FormatException("输入格式错误，请使用'小时 分钟'格式");
+                    }
+
+                    var now = DateTime.Now;
+                    var alarmTime = new DateTime(now.Year, now.Month, now.Day, hour, minute, 0);
+
+                    clock.SetNewAlarm(alarmTime);
+                    clock.Start();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"错误：{ex.Message}");
+                }
+            }
+        }
+    }
+}
